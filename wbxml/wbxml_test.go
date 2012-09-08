ff60e29dd294edf5b9079861e76b14b8b1a1be0f
Martin Gondermann <magicmonty@pagansoft.de>
2012-09-08 22:08:12 +0200

    Added example from http://www.w3.org/TR/wbxml/#_Toc443384926

--- wbxml/wbxml_test.go
+++ wbxml/wbxml_test.go
@@ -10,32 +10,6 @@ func TestDummy(t *testing.T) {
 
 }
 
-/*
-func _ExampleWBXMLDecode() {
-	var data []byte = make([]byte, 34)
-	data = []byte{
-		0x01, 0x01, 0x03, 0x00, 0x47, 0x46, 0x03, 0x20,
-		0x58, 0x20, 0x26, 0x20, 0x59, 0x00, 0x05, 0x03,
-		0x20, 0x58, 0x00, 0x02, 0x81, 0x20, 0x03, 0x3D,
-		0x00, 0x02, 0x81, 0x20, 0x03, 0x31, 0x20, 0x00,
-		0x01, 0x01}
-
-	var b *bytes.Buffer = bytes.NewBuffer(data)
-
-	var codeBook *CodeBook
-	*codeBook = NewCodeBook()
-	var codePage CodePage = NewCodePage("", 0)
-
-	codePage.AddTag("BR", 0x05)
-	codePage.AddTag("CARD", 0x06)
-	codePage.AddTag("XYZ", 0x07)
-
-	fmt.Println(Decode(b, codeBook))
-	// OUTPUT: <?xml version="1.0"?>
-	// <XYZ><CARD> X &amp; Y<BR/>X&nbsp;=&nbsp;1</CARD></XYZ>
-}
-*/
-
 const (
 	TAG_BR   byte = 0x05
 	TAG_CARD byte = 0x06
@@ -136,4 +110,24 @@ func ExampleDecodeEntity() {
 	fmt.Println(decoder.decodeEntity())
 	// OUTPUT: &#160;
 	// &#96;
+}
+
+// Example from http://www.w3.org/TR/wbxml/#_Toc443384926
+func _ExampleSimpleWBXMLDecode() {
+	fmt.Println(
+		Decode(
+			MakeDataBuffer(
+				WBXML_1_3, UNKNOWN_PI, CHARSET_UTF8, 0x00,
+				TAG_XYZ|TAG_HAS_CONTENT, TAG_CARD|TAG_HAS_CONTENT,
+				STR_I, ' ', 'X', ' ', '&', ' ', 'Y', 0x00,
+				TAG_BR,
+				STR_I, ' ', 'X', 0x00,
+				ENTITY, 0x81, 0x20,
+				STR_I, '=', 0x00,
+				ENTITY, 0x81, 0x20,
+				STR_I, '1', ' ', 0x00,
+				END, END),
+			MakeCodeBook()))
+	// OUTPUT: <?xml version="1.0"?>
+	// <XYZ><CARD> X &amp; Y<BR/> X&#160;=&#160;1 </CARD></XYZ>
 }
